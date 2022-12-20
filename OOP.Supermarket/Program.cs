51e532c61e5fc64bae8195ad3aeb2190403be417
otritsss <103356170+otritsss@users.noreply.github.com>
2022-12-20 14:13:30 +0400

    Alpha 1.0
    Made all the checks, even for 0 money.

--- OOP.Supermarket/Program.cs
+++ OOP.Supermarket/Program.cs
@@ -17,14 +17,12 @@ namespace OOP.Supermarket
 
     class Shop
     {
-        static private Random _random = new Random();
         private Queue<Buyer> _allBuyers = new Queue<Buyer>();
         private List<Product> _allProductsShop = new List<Product>() { new Product("Молоко", 60), new Product("Хлеб", 25), new Product("Творог", 90), new Product("Куриное филе", 150), new Product("Шоколад", 55), new Product("Сыр", 100), new Product("Жвачка", 25), new Product("Колбаса", 110), new Product("Салат", 39), new Product("Сок", 80) };
 
         public void WorkShop()
         {
-            int numberBuyer = 0;
-            int queueNumberBuyer = 0;
+            int numberBuyer = 1;
             CreateBuyers();
 
             while (_allBuyers.Count > 0)
@@ -34,8 +32,13 @@ namespace OOP.Supermarket
                 Console.WriteLine($"В очереди стоит {_allBuyers.Count} клиентов");
                 buyer.ShowInfo(numberBuyer);
 
-
-                if (buyer.SumBuy > buyer.Money)
+                if (buyer.Cart.Products.Count == 0)
+                {
+                    Console.WriteLine("У Вас не осталось товаров в корзине, потому что вы бедный чел. До свидания");
+                    _allBuyers.Dequeue();
+                    numberBuyer++;
+                }
+                else if (buyer.SumBuy > buyer.Money)
                 {
                     Console.WriteLine("Вам не хватает денег, поэтому мы уберем некоторые товары из корзины");
                     buyer.RemoveProduct();
@@ -63,32 +66,32 @@ namespace OOP.Supermarket
 
     class Buyer
     {
+        static private Random _random = new Random();
+
+        public Cart Cart { get; private set; } = new Cart();
         public int SumBuy { get; private set; }
         public int Money { get; private set; }
 
-        private Cart _cart = new Cart();
-
-        static private Random _random = new Random();
         public Buyer(List<Product> products)
         {
             int maxCountMoneyBuyer = 750;
             Money = _random.Next(maxCountMoneyBuyer);
 
-            int countProductsInCart = _random.Next(products.Count);
+            int countProductsInCart = _random.Next(1, products.Count);
 
             for (int i = 0; i < countProductsInCart; i++)
             {
                 int indexProduct = _random.Next(products.Count);
-                _cart.Products.Add(products[indexProduct]);
+                Cart.Products.Add(products[indexProduct]);
                 SumBuy += products[indexProduct].Price;
             }
         }
         public void RemoveProduct()
         {
-            int indexRemoveProduct = _random.Next(1, _cart.Products.Count);
-            Console.WriteLine($"Убрали {_cart.Products[indexRemoveProduct].Title}");
-            _cart.Products.RemoveAt(indexRemoveProduct);
-            SumBuy -= _cart.Products[indexRemoveProduct].Price;
+            int indexRemoveProduct = _random.Next(Cart.Products.Count);
+            Console.WriteLine($"Убрали {Cart.Products[indexRemoveProduct].Title}");
+            SumBuy -= Cart.Products[indexRemoveProduct].Price;
+            Cart.Products.RemoveAt(indexRemoveProduct);
 
         }
 
@@ -96,8 +99,8 @@ namespace OOP.Supermarket
         {
             Console.Write($"{number}-й клиент. Денег: {Money}\nКорзина: ");
 
-            for (int i = 0; i < _cart.Products.Count; i++)
-                Console.Write($"{_cart.Products[i].Title}, ");
+            for (int i = 0; i < Cart.Products.Count; i++)
+                Console.Write($"{Cart.Products[i].Title}, ");
 
             Console.WriteLine();
         }
