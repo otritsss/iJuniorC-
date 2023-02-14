7750cf91bd1ccfbfef6e5aad08d949587ff67e13
otritsss <urobertbtc@gmail.com>
2023-02-14 11:11:17 +0400

    Demo

--- OOP.War/Program.cs
+++ OOP.War/Program.cs
@@ -11,6 +11,10 @@ using System.Threading.Tasks;
 ///     
 /// Что придумал:
 ///     В каждом классе с помощью рандома задавать количество бойцов того или иного класса во взводе.
+///     
+///     14.02.2023
+///     
+/// 
 /// </summary>
 
 namespace OOP.War
@@ -42,20 +46,20 @@ namespace OOP.War
     {
         static private Random _random;
 
-        private Combatant[] _combatants = { new Sniper(), new MachineGunner(), new GrenadeLauncher(), new Doctor() };
+        private List<Combatant> _combatants = new List<Combatant>();
 
-        public void CreateCombatants()
+        public void CreateCombatants(Combatant combatans)
         {
-            for (int i = 0; i < _combatants[0].Count; i++)
+            for (int i = 0; i < combatans.Count; i++)
             {
-
+                _combatants.Add(combatans);
             }
         }
     }
 
     class Combatant
     {
-        static protected Random Random;
+        static protected Random Random = new Random();
 
         public int Count { get; protected set; }
         public int Health { get; protected set; } = 100;
@@ -67,7 +71,7 @@ namespace OOP.War
         public Combatant()
         {
             int maxArmor = 20;
-            //Armor = Random.Next(maxArmor);
+            Armor = Random.Next(maxArmor);
 
             int minCountCombatant = 5;
             int maxCountCombatant = 25;
