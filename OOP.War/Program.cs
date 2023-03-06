1b38dc3997212873dbbf434bc3dcb08714e278ed
otritsss <urobertbtc@gmail.com>
2023-03-06 09:03:43 +0400

    Demo 1.2 (06.03.23)
    I think I did a damage realization. I need to make iterative combat (by day)

--- OOP.War/Program.cs
+++ OOP.War/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 /// <summary>
 /// 1. Каким способом мне сделать создание радномного кол-ва каждого класса в отряде (не могут же всех быть столько же)
@@ -27,7 +28,7 @@ using System.Threading.Tasks;
 ///     3) Удалить класс 'Country' - YES
 ///     4) Сделать метод вывода характеристики в методе Platoon - YES
 ///     5) Нужно ли мне свойство CountComb.. Могу сделать проверку выживших в классе Platoon
-///     5) Нужно как-то передать список врагов (Возможно через метод)
+///     5) Нужно как-то передать список врагов (Возможно через метод). Ментор написал про IReadOnlyCollection
 /// 
 /// </summary>
 
@@ -55,7 +56,8 @@ namespace OOP.War
                 _platoonRussia.ShowInfo();
                 _platoonUsa.ShowInfo();
 
-
+                _platoonRussia.Fight(_platoonUsa);
+                _platoonUsa.Fight(_platoonRussia);
 
                 _platoonRussia.RemoveDead();
                 _platoonUsa.RemoveDead();
@@ -68,6 +70,8 @@ namespace OOP.War
 
     class Platoon
     {
+        const string GrenadeLauncherName = "Гранатометчик";
+
         static private Random _random = new Random();
         private List<Combatant> _combatants = new List<Combatant>();
 
@@ -103,11 +107,32 @@ namespace OOP.War
             return countCombatantOneGrade;
         }
 
-        public void Fight(List<Combatant> enemys)
+        public List<Combatant> ReturnRandomFighters(Combatant combatant)
+        {
+            List<Combatant> returnFighters = new List<Combatant>();
+            int enemyNumber = _random.Next(_combatants.Count);
+
+            if (combatant.GradeName != GrenadeLauncherName)
+            {
+                returnFighters.Add(_combatants[enemyNumber]);
+                return returnFighters;
+            }
+            else
+            {
+                for (int i = 0; i < combatant.CountEnemyTrapped; i++)
+                {
+                    returnFighters.Add(_combatants[enemyNumber++]);
+                }
+
+                return returnFighters;
+            }
+        }
+
+        public void Fight(Platoon enemy)
         {
             for (int i = 0; i < _combatants.Count; i++)
             {
-                _combatants[i].Attack(enemys);
+                _combatants[i].Attack(enemy.ReturnRandomFighters(_combatants[i]));
             }
         }
 
@@ -116,18 +141,13 @@ namespace OOP.War
             Console.WriteLine($"{NameCountry}: В живых - {_combatants.Count} бойцов " +
                     $"| Снайперов - {CountingCombatantOneGrade("Снайпер")}, " +
                     $"Пулеметчиков - {CountingCombatantOneGrade("Пулеметчик")}, Гранатомётчиков - {CountingCombatantOneGrade("Гранатометчик")}");
-
         }
 
         public void RemoveDead()
         {
             for (int i = 0; i < _combatants.Count; i++)
-            {
                 if (_combatants[i].Health <= 0)
-                {
                     _combatants.RemoveAt(i);
-                }
-            }
         }
     }
 
@@ -141,26 +161,22 @@ namespace OOP.War
             Armor = Random.Next(maxArmor);
         }
 
+        public int CountEnemyTrapped { get; protected set; }
         public int Count { get; protected set; }
         public int Health { get; protected set; } = 100;
         public int Damage { get; protected set; } = 20;
         public int Armor { get; protected set; }
-        public bool IsLive { get; protected set; } = true;
         public string GradeName { get; protected set; }
 
         public virtual void Attack(List<Combatant> enemys)
         {
-            int enemyNumber = Random.Next(enemys.Count);
-
-            enemys[enemyNumber].TakeDamage(Damage);
+            for (int i = 0; i < enemys.Count; i++)
+                enemys[i].TakeDamage(Damage);
         }
 
         public virtual void TakeDamage(int damage)
         {
-            if (Health > 0)
-                Health -= damage;
-            else
-                IsLive = false;
+            Health -= damage;
         }
 
         public void ShowInfo()
@@ -194,14 +210,14 @@ namespace OOP.War
         {
             GradeName = "Гранатометчик";
             Damage = 100;
+            CountEnemyTrapped = 3;
         }
 
         public override void Attack(List<Combatant> enemys)
         {
             int enemyNumber = Random.Next(enemys.Count);
-            int countEnemyTrapped = 3;
 
-            for (int i = 0; i < countEnemyTrapped; i++)
+            for (int i = 0; i < CountEnemyTrapped; i++)
                 enemys[enemyNumber++].TakeDamage(Damage);
         }
     }
